c3f2a818f6a8a4377431e794ba944175c272034f
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-04 19:38:16 +0500

    Task0

---
+++ Tyuiu.DmitrievLR.Sprint3.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DmitrievLR.Sprint3.Task0.V10.Lib
+{
+    public class DataService : ISprint3Task0V10
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double dd = 1;
+            for ( int i = startValue; i <=5; i++)
+            {
+                 dd *= Math.Pow(300 / (i + (Math.Pow(value, i))),i);
+            }
+
+            
+            
+            
+            return dd;
+        }
+    }
+}
