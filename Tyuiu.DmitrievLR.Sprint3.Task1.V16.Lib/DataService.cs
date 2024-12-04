61f5be09035d007cac91d6fa045fc34894493339
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-04 19:49:12 +0500

    Task1

---
+++ Tyuiu.DmitrievLR.Sprint3.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DmitrievLR.Sprint3.Task1.V16.Lib
+{
+    public class DataService : ISprint3Task1V16
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double dd = 1;
+            for (int i = startValue; i <= 5; i++)
+            {
+                dd += ( Math.Pow(value, 2) * Math.Sin(i) ) + 1;
+            }
+            
+            
+            return Math.Round(dd,3);
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint3.Task1.V16/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.DmitrievLR.Sprint3.Task1.V16.Lib;
+namespace Tyuiu.DmitrievLR.Sprint3.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("введите значение x: ");
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("введите начальное значение: ");
+            int startValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите конечное значение: ");
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumSeries(value, startValue, stopValue);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
