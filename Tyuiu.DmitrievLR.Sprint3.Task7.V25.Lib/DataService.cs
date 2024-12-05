c7f67d335f25366cf0d205e8e45248b0c64d7bd7
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-05 19:38:16 +0500

    Task7

---
+++ Tyuiu.DmitrievLR.Sprint3.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DmitrievLR.Sprint3.Task7.V25.Lib
+{
+    public class DataService : ISprint3Task7V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            Func<double, double> f = x => Math.Cos(x) + 4 * x / 2 - Math.Sin(x) * 3 * x;
+
+            double step = 1;
+            double[] values = new double[(int)((stopValue - startValue) / step) + 1];
+
+            int i = 0;
+            for (double x = startValue; x <= stopValue; x += step)
+            {
+                // Check for division by zero
+                if (x == 0)
+                {
+                    values[i] = 0;
+                }
+                else
+                {
+                    values[i] = Math.Round(f(x), 2);
+                }
+                i++;
+            }
+             
+            return values;
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint3.Task7.V25.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint3.Task7.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1() 
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
