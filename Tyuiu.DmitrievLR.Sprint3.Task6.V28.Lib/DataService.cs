b48e51dac2ade61b5ba2c9951c1f049e12446264
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-05 14:51:07 +0500

    Task6

---
+++ Tyuiu.DmitrievLR.Sprint3.Task6.V28.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DmitrievLR.Sprint3.Task6.V28.Lib
+{
+    public class DataService : ISprint3Task6V28
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                int divisorsCount = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        divisorsCount++;
+                        if (divisorsCount >= 7)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (divisorsCount < 7)
+                {
+                    count++;
+                }
+
+            }
+                return count;
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint3.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint3.Task6.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
